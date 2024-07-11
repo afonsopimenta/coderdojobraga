3ae869d9710dbbb980037c6b2ed31fbc472dfebe
Afonso Pimenta <pimenta.afonsosilva@gmail.com>
2024-07-11 14:10:40 +0100

    ui: add links to pages on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import Link from "next/link";
+
 const HomePage = () => {
   return (
-    <main className="grid min-h-dvh place-items-center">
+    <main className="grid min-h-dvh place-content-center gap-8">
       <h1 className="text-6xl font-bold">CoderDojo Braga</h1>
+      <div className="flex items-center justify-center gap-8">
+        <Link href="/sign-up" className="text-blue-500 underline">
+          Sign up
+        </Link>
+        <Link href="/sign-in" className="text-blue-500 underline">
+          Sign in
+        </Link>
+        <Link href="/api/sign-out" className="text-blue-500 underline">
+          Sign out
+        </Link>
+        <Link href="/whoami" className="text-blue-500 underline">
+          Who am I
+        </Link>
+        <Link href="/dashboard" className="text-blue-500 underline">
+          Dashboard
+        </Link>
+      </div>
     </main>
   );
 };
